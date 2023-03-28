6617cb5974cd249110b03c708fc538e61eb315b8
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-03-28 20:36:55 -0300

    Now the app is usefull
    Read the READ.ME

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JFaceRecognizer
  A face recognition app based on OpenCV Java for LP3 in UTFPR.
+
+# VERY IMPORTANT
+ Please, before use remember to refactor all the 'opencv' folder with your machine's 'opencv' folder.
+ Please, before use remember to trade the paths.
--- src/Main/MainGUI.java
+++ src/Main/MainGUI.java
@@ -33,37 +33,38 @@ public class MainGUI extends JDialog {
     Container cp;
     JPanel pnNorte = new JPanel();
     JPanel pnCentro = new JPanel();
+    JPanel pnSul = new JPanel();
     JButton btRun = new JButton("RUN");
     JButton btProcurar = new JButton("Procurar Imagem");
 
 ////////////////////////////////// - MUTÁVEL - //////////////////////////////////
     String[] lista_haar = {
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalcatface_extended.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_profile_face.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_smile.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_fullbody.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_upperbody.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_lowerbody.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_smile.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml",
-        "D:\\Mateus Cohuzer\\Desktop\\Geral\\opencv\\build\\etc\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml"
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalcatface_extended.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_profile_face.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_smile.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_fullbody.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_upperbody.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_lowerbody.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_smile.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml",
+        "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\opencv\\build\\etc\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml"
     };
 
     String output_img_path = "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\src\\imagem_com_faces.jpg";
     String std_img_path = "D:\\Mateus Cohuzer\\Documents\\NetBeansProjects\\JFaceRecog\\src\\standart.jpg";
 
     JComboBox<String> cbHaar = new JComboBox<>(lista_haar);
-    JLabel lbPath = new JLabel("Caminho:");
+    JLabel lbPath = new JLabel("Path:");
     JTextField tfPath = new JTextField(30);
     JLabel lbOutput = new JLabel();
     ImagemAjustada imagemAjustada = new ImagemAjustada();
 
     private JFileChooser caixaDeDialogo = new JFileChooser();
-    FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "jpeg", "png");
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("IMAGES", "jpg", "jpeg", "png");
     ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
     int tamX = 920; //w
-    int tamY = 620; //h
+    int tamY = 610; //h
     String caminho = "";
     List<String> texto = new ArrayList();
 
@@ -71,8 +72,10 @@ public class MainGUI extends JDialog {
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
+        pnNorte.setBackground(Color.WHITE);
         pnCentro.setBackground(Color.WHITE);
-
+        pnSul.setBackground(Color.WHITE);
+        
         caixaDeDialogo.setFileFilter(filter);
 
         ImageIcon icon = imagemAjustada.getImagemAjustada(std_img_path, tamX, tamY);
@@ -84,16 +87,18 @@ public class MainGUI extends JDialog {
 
         cp.add(pnNorte, BorderLayout.NORTH);
         cp.add(pnCentro, BorderLayout.CENTER);
+        cp.add(pnSul, BorderLayout.SOUTH);
 
 //        pnCentro.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 //        pnNorte.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-//        pnNorte2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         pnNorte.setLayout(new FlowLayout(FlowLayout.CENTER));
+        pnSul.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         pnNorte.add(lbPath);
         pnNorte.add(tfPath);
         pnNorte.add(btProcurar);
         pnNorte.add(btRun);
+        pnSul.add(cbHaar);
 
         tfPath.setEditable(false);
 
